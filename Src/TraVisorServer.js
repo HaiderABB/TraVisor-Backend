1f5bebdee06e3b3b4d6ad635639a449e1ba119ca
HaiderABB <haider.a.moazzam@gmail.com>
2024-07-30 15:13:49 +0500

    dotenv issue fix

--- Src/TraVisorServer.js
+++ Src/TraVisorServer.js
@@ -3,7 +3,9 @@ const MongoConnection = require('./Config/DBConnection');
 const AuthRouter = require('./Routes/AuthRouter');
 const bodyParser = require('body-parser');
 const FlightsRouter = require('./Routes/FlightsRouter')
-require('dotenv').config('../.env');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
+
 
 // Async server for handling database connections
 const connection = MongoConnection();
