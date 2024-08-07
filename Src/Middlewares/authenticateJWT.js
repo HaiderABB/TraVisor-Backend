b2c8b4791edd7f3636e7c8a3a41b43760c88293a
HaiderABB <haider.a.moazzam@gmail.com>
2024-08-07 11:43:54 +0500

    Authenticate JWT Middleware

--- Src/Middlewares/authenticateJWT.js
+++ Src/Middlewares/authenticateJWT.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-export const AuthenticateJWT = async (req, res) => {
+const AuthenticateJWT = async (req, res, next) => {
+  const token = req.body.jwt_token;
+  if (!token) {
+    console.log(!token);
+    res.status(400).json({ authenticated: false, token: false, expired: falseS });
+  }
+  else {
+    try {
+      const decoded = jwt.verify(token, 'TRAVEL%PLANNER%WEBSITE');
 
+    } catch (err) { res.status(404).json({ authenticated: false, token: false, expired: true }) }
+  }
 }
+
+AuthenticateJWT();
